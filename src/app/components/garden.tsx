b48fd67595172478fdae60baf1c53e6a0933d093
Elias Shemsu <echosierran@gmail.com>
2024-05-27 20:16:51 +0300

    Spline embeded garden

---
+++ src/app/components/garden.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Spline from "@splinetool/react-spline";
+
+const Garden = () => {
+  return (
+    <div>
+      <Spline scene="https://prod.spline.design/l2rbywesQMvwps3q/scene.splinecode" />
+    </div>
+  );
+};
+
+export default Garden;
